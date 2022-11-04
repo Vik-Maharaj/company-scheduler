2b6d0fd73ca43ee622d4e42e9c31ee9ac22f60ba
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-11-04 09:23:12 -0400

    Add home page component

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import HeaderSection from "../../components/HeaderSection";
+import Footer from "../../components/Footer";
+
+export default function HomePage() {
+  return (
+    <>
+      <HeaderSection page={"home"} />
+      <Footer />
+    </>
+  );
+}
