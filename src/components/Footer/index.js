06bb2f455188ec3f3751ede46714b1af388117cc
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-11-13 12:22:00 -0500

    Update footer index.js to include onclicks

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import { Container, FooterLogo, NavButtons, NavButton } from "./style";
+import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../../contexts/UserContext";
 
@@ -6,12 +8,26 @@ export default function Footer() {
   const { openAuthenticationModal, loadingUserValidation, userIsLoggedIn } =
     useContext(UserContext);
 
+  const navigate = useNavigate();
 
   return (
     <Container>
       <FooterLogo src={logo} alt="" />
       <NavButtons>
+        <NavButton onClick={() => navigate("/")}>Home Page</NavButton>
+        <NavButton onClick={() => navigate("/Services")}>Services</NavButton>
+        <NavButton onClick={() => navigate("/Contact")}>Contact</NavButton>
 
+        {loadingUserValidation ? (
+          <NavButton>
+          </NavButton>
+        ) : (
+          <>
+            {userIsLoggedIn ? (
+              <NavButton>
+              </NavButton>
+            )}
+          </>
       </NavButtons>
     </Container>
   );
