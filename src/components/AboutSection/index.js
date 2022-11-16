6a65ab8852108e1e4734658fb88f9351a99d22fc
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-11-16 09:18:26 -0500

    Add about section component placeholder

---
+++ src/components/AboutSection/index.js
@@ -0,0 +1,27 @@
+import {
+  Container,
+  AboutContainer,
+  InfoContainer,
+  Paragraph,
+} from "./style";
+
+export default function AboutSection() {
+  return (
+    <Container>
+      <AboutContainer>
+
+        <InfoContainer>
+          <Description>
+            <Paragraph>
+
+            </Paragraph>
+            <Paragraph>
+
+            </Paragraph>
+          </Description>
+          <SocialIcons />
+        </InfoContainer>
+      </AboutContainer>
+    </Container>
+  );
+}
