7be1a4f9d5ad572ac31fa27cc46cd5f7359aa0ad
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-05 10:07:06 -0500

    Include categories, edit and delete

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,12 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../../contexts/UserContext";
+import * as api from "../../services/api";
 import {
-  StyledModal,
-  InputsForm,
   Input,
+  Button,
 } from "./style";
 
-export default function Category({
+export default function CategoryModal({
+  categoryModalIsOpen,
   categoryData,
   type,
 }) {
@@ -20,21 +21,59 @@ export default function Category({
     } else {
       setTitle("");
     }
+  }, [type, categoryModalIsOpen]);
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (type === "create") return createCategory();
+    if (type === "delete") return deleteCategory();
+    if (type === "edit") return editCategory();
+  }
+
+  async function createCategory() {
+    const response = await api.createCategory(token, { title });
+    if (response.status === 201) return handleSuccess("Category");
+
+    return handleError(response.data);
+  }
+
+  async function deleteCategory() {
+    const response = await api.deleteCategory(token, categoryData._id);
+    if (response.status === 200) return handleSuccess("Category");
+
+    return handleError(response.data);
+  }
+
+  }
+
 
   return (
-    <StyledModal>
+    <StyledModal
+      isOpen={categoryModalIsOpen}
+      ariaHideApp={false}
+      onRequestClose={() => closeModal()}
+      style={modalStyles}
+    >
+      <IoClose className="close-icon" onClick={() => closeModal()} />
       <InputsForm>
         {type !== "delete" && (
           <Input
             name="title"
             type="text"
-            placeholder="Título"
+            placeholder=""
             onChange={(e) => setTitle(e.target.value)}
             value={title}
             required
           />
         )}
+
+        <ActionButtons>
+          <Button type="button" onClick={() => closeModal()}>
+          </Button>
+          <Button onClick={(e) => handleSubmit(e)}></Button>
+        </ActionButtons>
+      </InputsForm>
     </StyledModal>
-    
   );
-}
