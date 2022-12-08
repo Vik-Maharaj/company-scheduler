4e9afdadd0e556f0d06b22c9d0cc9d42a861c7a8
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-08 14:11:56 -0500

    Add input fields

--- src/components/ProfileConfig/index.js
+++ src/components/ProfileConfig/index.js
@@ -2,6 +2,8 @@ import { useContext, useState } from "react";
 import UserContext from "../../contexts/UserContext";
 import {
   Container,
+  Title,
+  InputContainer,
 } from "./style";
 
 export default function ProfileConfigSection({ setDeleteAccountModalIsOpen }) {
@@ -10,6 +12,35 @@ export default function ProfileConfigSection({ setDeleteAccountModalIsOpen }) {
 
   return (
     <Container>
+      <Title>Profile</Title>
+      <InputContainer>
+        <FieldName>Name</FieldName>
+        <Input
+          placeholder={change === "name" ? "" : userData?.name}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          disabled={change === "name" && !isLoading ? false : true}
+        />
+        <Action onClick={() => changeInformation("name")}>
+          {change === "name" ? (
+          )}
+        </Action>
+      </InputContainer>
+      <InputContainer>
+        <FieldName>Phone</FieldName>
+        <StyledNumberFormat
+          placeholder={change === "phone" ? "" : userData?.phone}
+          onChange={(e) => setPhone(e.target.value)}
+          format={"(##) #####-####"}
+          value={phone}
+          disabled={change === "phone" && !isLoading ? false : true}
+        />
+        <Action onClick={() => changeInformation("phone")}>
+          {change === "phone" ? (
+            isLoading ? (
+          )}
+        </Action>
+      </InputContainer>
 
     </Container>
   );
