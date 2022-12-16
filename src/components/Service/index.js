68989d4a51844feb417f5084c8daed9bddb7f153
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-16 10:15:57 -0500

    Add create service function

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -12,6 +12,11 @@ import {
 } from "./style";
 
 export default function ServiceModal({
+  serviceModalIsOpen,
+  setServiceModalIsOpen,
+  categoryData,
+  serviceData,
+  type,
 }) {
   const rangeInputValues = [
     "15min",
@@ -28,7 +33,63 @@ export default function ServiceModal({
     description: "",
   });
 
+  const [duration, setDuration] = useState("15min");
 
+  useEffect(() => {
+    if (type === "edit") {
+      setFormData({
+        name: serviceData.name,
+        price: serviceData.price,
+        description: serviceData.description,
+      });
+      setDuration(serviceData.duration);
+    } else {
+      setFormData({ name: "", price: "", description: "" });
+      setDuration("15min");
+    }
+  }, [type, serviceModalIsOpen]);
+
+  function closeModal() {
+    document.body.style.overflow = "unset";
+    setServiceModalIsOpen(false);
+    setFormData({
+      name: "",
+      price: "",
+      description: "",
+    });
+    setDuration("15min");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (type === "create") return createService();
+    if (type === "delete") return deleteService();
+    if (type === "edit") return editService();
+  }
+
+  async function createService() {
+    const response = await api.createService(token, categoryData._id, {
+      ...formData,
+      duration,
+    });
+    if (response.status === 201) return handleSuccess("success");
+
+    return handleError(response.data);
+  }
+
+
+  function handleError(responseData) {
+    if (responseData) return toast.error(responseData);
+
+    return toast.error(
+      "Error",
+    );
+  }
+
+  function handleFormData(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
   return (
     <StyledModal>
