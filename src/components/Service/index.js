b7fa146b1255882ed122a6dcff84f0a2c2859a6f
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-16 09:45:23 -0500

    Add service modal function and export function

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -10,3 +10,28 @@ import {
   Button,
   modalStyles
 } from "./style";
+
+export default function ServiceModal({
+}) {
+  const rangeInputValues = [
+    "15min",
+    "30min",
+    "45min",
+    "1h"
+  ];
+
+  const { token } = useContext(UserContext);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    description: "",
+  });
+
+
+
+  return (
+    <StyledModal>
+    </StyledModal>
+  );
+}
