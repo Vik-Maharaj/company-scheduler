2b0ca57d7d77096c4e762416e53b9ce8cd4f5ff8
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-20 16:18:27 -0500

    Add container

--- src/components/CommentsSection/index.js
+++ src/components/CommentsSection/index.js
@@ -7,8 +7,17 @@ import {
   Author,
 } from "./style";
 import { useState } from "react";
+import commentsArray from "../../data/CommentsSection";
 
 export default function CommentsSection() {
   const [cursorStyle, setCursorStyle] = useState("pointer");
 
+  return (
+    <Container>
+      <Title>Comments</Title>
+      <Spacer />
+      <StyledScrollContainer>
+      </StyledScrollContainer>
+    </Container>
+  );
 }
