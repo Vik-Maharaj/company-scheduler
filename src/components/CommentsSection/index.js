5376ecb14c08993e10360f12f547b7206cfeaeeb
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-12-20 15:20:12 -0500

    Add comment component

---
+++ src/components/CommentsSection/index.js
@@ -0,0 +1,14 @@
+import {
+  Container,
+  Title,
+  StyledScrollContainer,
+  Comment,
+  Description,
+  Author,
+} from "./style";
+import { useState } from "react";
+
+export default function CommentsSection() {
+  const [cursorStyle, setCursorStyle] = useState("pointer");
+
+}
