ceecc270275da8559a0e2346549d67cfe8d0846c
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-01-10 11:15:25 -0500

    Add new user modal index

---
+++ src/components/UpdateNewUserModal/index.js
@@ -0,0 +1,8 @@
+import { useContext, useState } from "react";
+import UserContext from "../../contexts/UserContext";
+import NumberFormat from "react-number-format";
+import * as api from "../../services/api";
+import {
+};
+
+
