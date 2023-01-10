40db10bc535f41d521cedc966883ca1f12c334c5
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-01-10 15:26:44 -0500

    Add New User Modal function

--- src/components/UpdateNewUserModal/index.js
+++ src/components/UpdateNewUserModal/index.js
@@ -3,6 +3,13 @@ import UserContext from "../../contexts/UserContext";
 import NumberFormat from "react-number-format";
 import * as api from "../../services/api";
 import {
-};
+  StyledModal,
+  Title,
+  InputsForm,
+  Button,
+} from "./style";
 
-
+export default function UpdateNewUserModal() {
+  const { token, setToken, userIsNewUser, setUserIsNewUser } =
+    useContext(UserContext);
+}
