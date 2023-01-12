c18cadd0f9a686192236eb6ff088ec7f2918987a
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-01-12 11:55:29 -0500

    Add read more function

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -4,3 +4,18 @@ import {
   Title,
   Description
 } from "./style";
+import { IoClose } from "react-icons/io5";
+import { useContext } from "react";
+import UserContext from "../../contexts/UserContext";
+
+export default function ReadMore({
+  setReadMoreIsOpen,
+}) {
+  const { token, setAuthenticationIsOpen } = useContext(UserContext);
+
+  function closeModal() {
+    document.body.style.overflow = "unset";
+    setReadMoreIsOpen(false);
+  }
+
+}
