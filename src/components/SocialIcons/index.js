cb323a212ae92d109eae49e1c9e26be948150ffa
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-01-27 08:46:56 -0500

    Update social icons component to include socials

--- src/components/SocialIcons/index.js
+++ src/components/SocialIcons/index.js
@@ -1,4 +1,19 @@
 import { Container } from "./style";
 
 export default function SocialIcons() {
+  const REACT_APP_INSTAGRAM_LINK = process.env.REACT_APP_INSTAGRAM_LINK;
+  const REACT_APP_FACEBOOK_LINK = process.env.REACT_APP_FACEBOOK_LINK;
+
+  return (
+    <Container>
+      <AiOutlineInstagram
+        className="icon"
+        onClick={() => window.open(REACT_APP_INSTAGRAM_LINK, "_blank")}
+      />
+      <RiFacebookFill
+        className="icon"
+        onClick={() => window.open(REACT_APP_FACEBOOK_LINK, "_blank")}
+      />
+    </Container>
+  );
 }
